88109b8c7b4abea1dccb65b58888e1f354e4616f
william.cheng <chsh_mail@163.com>
2013-05-10 10:58:21 +0800

    优化CoverFlow.

--- sample/src/android/support/mdroid/widget/CoverFlow.java
+++ sample/src/android/support/mdroid/widget/CoverFlow.java
@@ -12,10 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * This code is base on the Android Gallery widget and was Created
+ * 
+ * This code is base on the Android Gallery widget and was Created 
  * by Neil Davies neild001 'at' gmail dot com to be a Coverflow widget
- *
+ * 
  * @author Neil Davies
  */
 package android.support.mdroid.widget;
@@ -23,54 +23,53 @@ package android.support.mdroid.widget;
 import android.content.Context;
 import android.graphics.Camera;
 import android.graphics.Matrix;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Transformation;
 import android.widget.Gallery;
 import android.widget.ImageView;
-import android.widget.SpinnerAdapter;
 
 @SuppressWarnings("deprecation")
 public class CoverFlow extends Gallery {
 
     /**
-     * Graphics Camera used for transforming the matrix of ImageViews.
+     * Graphics Camera used for transforming the matrix of ImageViews
      */
-    private final Camera mCamera = new Camera();
+    private Camera mCamera = new Camera();
 
     /**
-     * The maximum angle the Child ImageView will be rotated by.
+     * The maximum angle the Child ImageView will be rotated by
      */
-    private int mMaxRotationAngle = 45;
+    private int mMaxRotationAngle = 60;
 
     /**
-     * The maximum zoom on the centre Child.
+     * The maximum zoom on the centre Child
      */
     private int mMaxZoom = -120;
-    
-    private float mZAxis = 100.0f;
 
     /**
-     * The Centre of the Coverflow.
+     * The Centre of the Coverflow
      */
     private int mCoveflowCenter;
 
-    public CoverFlow(final Context context) {
+    public CoverFlow(Context context) {
         super(context);
         this.setStaticTransformationsEnabled(true);
     }
 
-    public CoverFlow(final Context context, final AttributeSet attrs) {
-        this(context, attrs, android.R.attr.galleryStyle);
+    public CoverFlow(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setStaticTransformationsEnabled(true);
     }
 
-    public CoverFlow(final Context context, final AttributeSet attrs, final int defStyle) {
+    public CoverFlow(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         this.setStaticTransformationsEnabled(true);
     }
 
     /**
-     * Get the max rotational angle of the image.
+     * Get the max rotational angle of the image
      * 
      * @return the mMaxRotationAngle
      */
@@ -78,23 +77,18 @@ public class CoverFlow extends Gallery {
         return mMaxRotationAngle;
     }
 
-    @Override
-    public void setAdapter(final SpinnerAdapter adapter) {
-        super.setAdapter(adapter);
-    }
-
     /**
-     * Set the max rotational angle of each image.
+     * Set the max rotational angle of each image
      * 
      * @param maxRotationAngle
      *            the mMaxRotationAngle to set
      */
-    public void setMaxRotationAngle(final int maxRotationAngle) {
+    public void setMaxRotationAngle(int maxRotationAngle) {
         mMaxRotationAngle = maxRotationAngle;
     }
 
     /**
-     * Get the Max zoom of the centre image.
+     * Get the Max zoom of the centre image
      * 
      * @return the mMaxZoom
      */
@@ -103,25 +97,17 @@ public class CoverFlow extends Gallery {
     }
 
     /**
-     * Set the max zoom of the centre image.
+     * Set the max zoom of the centre image
      * 
      * @param maxZoom
      *            the mMaxZoom to set
      */
-    public void setMaxZoom(final int maxZoom) {
+    public void setMaxZoom(int maxZoom) {
         mMaxZoom = maxZoom;
     }
 
-    public float getmZAxis() {
-        return mZAxis;
-    }
-
-    public void setmZAxis(float mZAxis) {
-        this.mZAxis = mZAxis;
-    }
-    
     /**
-     * Get the Centre of the Coverflow.
+     * Get the Centre of the Coverflow
      * 
      * @return The centre of this Coverflow.
      */
@@ -130,11 +116,11 @@ public class CoverFlow extends Gallery {
     }
 
     /**
-     * Get the Centre of the View.
+     * Get the Centre of the View
      * 
      * @return The centre of the given view.
      */
-    private static int getCenterOfView(final View view) {
+    private static int getCenterOfView(View view) {
         return view.getLeft() + view.getWidth() / 2;
     }
 
@@ -143,9 +129,7 @@ public class CoverFlow extends Gallery {
      * 
      * @see #setStaticTransformationsEnabled(boolean)
      */
-    @Override
-    protected boolean getChildStaticTransformation(final View child, final Transformation t) {
-
+    protected boolean getChildStaticTransformation(View child, Transformation t) {
         final int childCenter = getCenterOfView(child);
         final int childWidth = child.getWidth();
         int rotationAngle = 0;
@@ -156,16 +140,27 @@ public class CoverFlow extends Gallery {
         if (childCenter == mCoveflowCenter) {
             transformImageBitmap((ImageView) child, t, 0);
         } else {
-            rotationAngle = (int) ((float) (mCoveflowCenter - childCenter) / childWidth * mMaxRotationAngle);
+            rotationAngle = (int) (((float) (mCoveflowCenter - childCenter) / childWidth) * mMaxRotationAngle);
             if (Math.abs(rotationAngle) > mMaxRotationAngle) {
-                rotationAngle = rotationAngle < 0 ? -mMaxRotationAngle : mMaxRotationAngle;
+                rotationAngle = (rotationAngle < 0) ? -mMaxRotationAngle : mMaxRotationAngle;
             }
             transformImageBitmap((ImageView) child, t, rotationAngle);
         }
-
+        
         return true;
     }
-
+    
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            int count = getChildCount();
+            for (int i = 0; i < count; i++) {
+                getChildAt(i).invalidate();
+            }
+        }
+    }
+    
     /**
      * This is called during layout when the size of this view has changed. If you were
      * just added to the view hierarchy, you're called with the old values of 0.
@@ -179,14 +174,13 @@ public class CoverFlow extends Gallery {
      * @param oldh
      *            Old height of this view.
      */
-    @Override
-    protected void onSizeChanged(final int w, final int h, final int oldw, final int oldh) {
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         mCoveflowCenter = getCenterOfCoverflow();
         super.onSizeChanged(w, h, oldw, oldh);
     }
 
     /**
-     * Transform the Image Bitmap by the Angle passed.
+     * Transform the Image Bitmap by the Angle passed
      * 
      * @param imageView
      *            ImageView the ImageView whose bitmap we want to rotate
@@ -195,27 +189,25 @@ public class CoverFlow extends Gallery {
      * @param rotationAngle
      *            the Angle by which to rotate the Bitmap
      */
-    private void transformImageBitmap(final ImageView child, final Transformation t, final int rotationAngle) {
+    private void transformImageBitmap(ImageView child, Transformation t, int rotationAngle) {
         mCamera.save();
         final Matrix imageMatrix = t.getMatrix();
-
-        final int height = child.getMeasuredHeight();
-        final int width = child.getMeasuredWidth();
+        final int imageHeight = child.getLayoutParams().height;
+        final int imageWidth = child.getLayoutParams().width;
         final int rotation = Math.abs(rotationAngle);
 
-        mCamera.translate(0.0f, 0.0f, mZAxis);
+        mCamera.translate(0.0f, 0.0f, 100.0f);
 
         // As the angle of the view gets less, zoom in
         if (rotation < mMaxRotationAngle) {
-            final float zoomAmount = (float) (mMaxZoom + rotation * 4);
+            float zoomAmount = (float) (mMaxZoom + (rotation * 1.5));
             mCamera.translate(0.0f, 0.0f, zoomAmount);
         }
 
         mCamera.rotateY(rotationAngle);
         mCamera.getMatrix(imageMatrix);
-
-        imageMatrix.preTranslate(-(width / 2.0f), -(height / 2.0f));
-        imageMatrix.postTranslate((width / 2.0f), (height / 2.0f));
+        imageMatrix.preTranslate(-(imageWidth / 2), -(imageHeight / 2));
+        imageMatrix.postTranslate((imageWidth / 2), (imageHeight / 2));
         mCamera.restore();
     }
 
